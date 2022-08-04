37f0a016fce5bcc8de804579bffab1daf7c71eb2
Hoang-Dac-Hung <hdhung.data@gmail.com>
2022-08-05 04:25:14 +0700

    init server nodejs crud

---
+++ server/routes/router.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const route = express.Router()
+const services = require('../services/render')
+const controller = require('../controller/controller')
+/**
+ * @description ROOT Route
+ * @Method GET /
+ */
+route.get('/',services.homeRoutes)
+
+/**
+ * @description add user
+ * @Method GET /add-user
+ */
+route.get('/add-user',services.add_user)
+
+/**
+ * @description update user
+ * @Method GET /update-user
+ */
+route.get('/update-user',services.update_user)
+
+route.post('/api/users',controller.create)
+route.get('/api/users',controller.find)
+route.put('/api/users/:id',controller.update)
+route.delete('/api/users/:id',controller.delete)
+
+
+module.exports = route
